06037dc7aadfc7e4885d5347126ce74e5f3170de
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-08-31 23:55:23 -0300

    Check if logged in

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,15 +11,27 @@ import { AuthRouter } from './AuthRouter';
 import { firebase } from '../firebase/firebase-config';
 import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
+import { useState } from 'react';
 export const AppRouter = () => {
   const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
       }
+      setChecking(false);
     });
-  }, [dispatch]);
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return <h1>Please wait...</h1>;
+  }
+
   return (
     <Router>
       <div>

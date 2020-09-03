37186dc6be210e08465fcdee16c04b59ec2a9292
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-02 21:49:50 -0300

    Active note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -12,6 +12,14 @@ export const startNewNote = () => {
       date: new Date().getTime(),
     };
     const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
-    console.log(doc);
+    dispatch(activeNote(doc.id, newNote));
   };
 };
+
+export const activeNote = (id, note) => ({
+  type: types.notesActive,
+  payload: {
+    id,
+    ...note,
+  },
+});

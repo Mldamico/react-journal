43d9ed66e192ecee5076b9432bae586b3a849fba
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-04 20:20:34 -0300

    Editing notes

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
@@ -19,6 +20,10 @@ export const NoteScreen = () => {
       activeId.current = note.id;
     }
   }, [note, reset]);
+
+  useEffect(() => {
+    dispatch(activeNote(formValues.id, { ...formValues }));
+  }, [formValues, dispatch]);
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
@@ -29,6 +34,7 @@ export const NoteScreen = () => {
           className='notes__title-input'
           autoComplete='off'
           value={title}
+          name='title'
           onChange={handleInputChange}
         />
         <textarea

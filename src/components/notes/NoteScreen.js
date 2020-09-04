0bfd3c948559a1af9448dc3f45f0995a19cd1df2
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-04 19:38:22 -0300

    Set active note

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
+  const [formValues, handleInputChange] = useForm(note);
+
+  const { body, title, url } = formValues;
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
@@ -11,17 +18,24 @@ export const NoteScreen = () => {
           placeholder='Some awesome title'
           className='notes__title-input'
           autoComplete='off'
+          value={title}
+          onChange={handleInputChange}
         />
         <textarea
           placeholder='What happened today?'
           className='notes__textarea'
+          value={body}
+          name='body'
+          onChange={handleInputChange}
         ></textarea>
-        <div className='notes__image'>
-          <img
-            src='https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg'
-            alt='Imagen'
-          />
-        </div>
+        {url && (
+          <div className='notes__image'>
+            <img
+              src='https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg'
+              alt='Imagen'
+            />
+          </div>
+        )}
       </div>
     </div>
   );

c4d9db5782d18f6e332eddc58cc86e7b46eb1448
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-06 19:35:28 -0300

    Remove note

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { activeNote } from '../../actions/notes';
+import { activeNote, startDeleting } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
@@ -24,6 +24,10 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch(activeNote(formValues.id, { ...formValues }));
   }, [formValues, dispatch]);
+
+  const handleDelete = () => {
+    dispatch(startDeleting(note.id));
+  };
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
@@ -50,6 +54,9 @@ export const NoteScreen = () => {
           </div>
         )}
       </div>
+      <button className='btn btn-danger' onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
